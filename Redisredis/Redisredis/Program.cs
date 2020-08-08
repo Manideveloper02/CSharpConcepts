f9f7809d27e08125ed510c70758a46aae3ac0445
Manideveloper02 <64354799+Manideveloper02@users.noreply.github.com>
2020-08-08 18:16:50 +0530

    Commit Redis Concept (in-memory cache )

---
+++ Redisredis/Redisredis/Program.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Redisredis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //REDIS
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+            if (!db.KeyExists("Name"))
+            {
+                db.KeyDelete("Name");
+                db.StringSet("Name", "Mani");
+            }
+            Console.WriteLine(db.StringGet("Name"));
+        }
+
+    }
+}
